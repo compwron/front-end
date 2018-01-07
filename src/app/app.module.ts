ab8c8ae9b7ee1ef12953815ef810725ddc06c834
geo <gg@pridepocket.org>
2018-01-07 14:17:21 -0800

    Components for pp layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Layout
+import {FooterComponent} from "./layout/footer.component";
+import {NavComponent} from "./layout/nav.component";
+
 
 import { AppComponent } from './app.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
