67f462b8f1237b6a56120612f1ffc08d1cb672db
Cortney <cortney@machinevantage.com>
2018-01-07 17:33:17 -0800

    add public campaigns page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import {RouterModule, Routes} from '@angular/router'
 // Layout
 import {FooterComponent} from "./layout/footer.component";
 import {NavComponent} from "./layout/nav.component";
+
+//style guide
 import { ButtonsComponent } from './sg/buttons/buttons.component';
 import { FormsComponent } from './sg/forms/forms.component';
 
 
 import { AppComponent } from './app.component';
+import { CampaignsComponent } from './components/campaigns/campaigns.component';
 
 
 const appRoutes: Routes = [
   {path:'sg/buttons', component:ButtonsComponent}
   {path:'sg/forms', component:FormsComponent}
+  {path:'campaigns', component:CampaignsComponent}
 ]
 
 @NgModule({
@@ -23,7 +27,8 @@ const appRoutes: Routes = [
     NavComponent,
     ButtonsComponent,
     FooterComponent,
-    FormsComponent
+    FormsComponent,
+    CampaignsComponent
   ],
   imports: [
     BrowserModule,
