c135ba8e758d81b778e600f7e0823d76fa76ce54
Cortney <cortney@machinevantage.com>
2018-01-07 16:53:50 -0800

    routed to style guide buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router'
 
 // Layout
 import {FooterComponent} from "./layout/footer.component";
 import {NavComponent} from "./layout/nav.component";
+import { ButtonsComponent } from './sg/buttons/buttons.component';
 
 
 import { AppComponent } from './app.component';
 
+const appRoutes: Routes = [
+  {path:'sg/buttons', component:ButtonsComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
+    ButtonsComponent,
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
