896eb2f3816cdd0645f14920aad747e5c8e1c0c1
Cortney <cortney@machinevantage.com>
2018-02-15 13:20:32 -0800

    sign up form works

--- src/app/components/authorization/signup/signup.component.ts
+++ src/app/components/authorization/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  name:string;
+  email:string;
+  password:string;
 
+  onSignUp(form:NgForm) {
+    console.log(form.value)
+  }
   constructor() { }
 
   ngOnInit() {
