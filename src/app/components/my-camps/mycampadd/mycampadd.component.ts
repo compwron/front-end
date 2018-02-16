f669edd32a833ae7b087c6c3cc67c549ecbcd863
Cortney <cortney@machinevantage.com>
2018-02-15 16:02:30 -0800

    add campaign form works

--- src/app/components/my-camps/mycampadd/mycampadd.component.ts
+++ src/app/components/my-camps/mycampadd/mycampadd.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-mycampadd',
@@ -6,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mycampadd.component.scss']
 })
 export class MycampaddComponent implements OnInit {
+  campName:string;
+  campType:string;
+  goal:number;
+  noDate:boolean;
+  date:string;
+  welMessage:string;
+  item1:string;
+  item2:string;
+  item3:string;
+  thankYou:string;
+  fEmail:string;
+  eMessage:string;
+  socialShare:boolean;
 
+  onAddCamp(form: NgForm) {
+    console.log(form.value)
+  }
   constructor() { }
 
   ngOnInit() {
