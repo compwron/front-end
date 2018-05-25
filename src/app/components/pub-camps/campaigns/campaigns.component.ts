481a2d544da0948a3153b0881ce6c82f691c1e11
cjohnson6382 <cjohnson6382@gmail.com>
2018-05-24 18:12:11 -0700

    wired up the sort feature on campaigns page

--- src/app/components/pub-camps/campaigns/campaigns.component.ts
+++ src/app/components/pub-camps/campaigns/campaigns.component.ts
@@ -16,6 +16,53 @@ export class CampaignsComponent implements OnInit {
 	
 	ngOnInit() { this.getCampaigns() }
 
+	sorter = {
+		"newest": (a, b) => {
+			let ac, bc
+			
+			if (!a.end) ac = 0
+			else ac = a.end.valueOf()
+			if (!b.end) bc = 0
+			else bc = b.end.valueOf()
+
+			return ac - bc
+		},
+		"popularity": (a, b) => {
+			let ac, bc
+			if (!a.received) ac = 0
+			else ac = Object.values(a.received).length
+			
+			if (!b.received) bc = 0
+			else bc = Object.values(b.received).length
+			
+			console.log(ac, bc, a.name, b.name)
+			
+			return ac - bc
+		},
+		"ending soonest": (a, b) => {
+			let ac, bc
+			
+			if (!a.end) ac = 0
+			else ac = a.end.valueOf()
+			if (!b.end) bc = 0
+			else bc = b.end.valueOf()
+
+			console.log(ac, bc, a.name, b.name)
+
+			return ac - bc
+		},
+		"closest to goal": (a, b) => a.current/a.goal - b.current/b.goal
+	}
+
+	sort (key) {
+		console.log(this.campaigns.map(c => c.name))
+		
+		// console.log(key)
+		this.campaigns.sort(this.sorter[key])
+		
+		console.log(this.campaigns.map(c => c.name))
+	}
+
 	getCampaigns (): void {
 		this.campaignService.subscribeCampaigns({ field: "active", operator: "==", value: true })
 			.subscribe(
